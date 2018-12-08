fba08cef7e657aa56322a55a0cf8c54835d69ff3
GimoXie <232571189@qq.com>
2018-12-08 22:13:28 +0800

    代码提交

---
+++ zeus-db/src/main/java/io/gimo/zeus/db/dao/zeusdb/SysPermissionMapper.java
@@ -0,0 +1,11 @@
+package io.gimo.zeus.db.dao.zeusdb;
+
+import io.gimo.zeus.db.dao.BaseMapper;
+import io.gimo.zeus.db.model.zeusdb.SysPermissionDO;
+import io.gimo.zeus.db.model.zeusdb.SysPermissionExample;
+
+/**
+ * SysPermissionMapper继承基类
+ */
+public interface SysPermissionMapper extends BaseMapper<SysPermissionDO, Long, SysPermissionExample> {
+}
